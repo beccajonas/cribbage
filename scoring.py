d92790ff78a71c079b2d29419fec720be27d2765
Becca Jonas <beccajonas3@gmail.com>
2024-01-06 12:40:20 -0500

    edit print statements

--- scoring.py
+++ scoring.py
@@ -25,7 +25,10 @@ class Scoring:
 
         for subset_size in range(1, len(card_list) + 1):
             for subset in itertools.combinations(card_list, subset_size):
+                subset_expression = " + ".join(f"{card.value} {card.suit}" for card in subset)
+
                 if sum(card.points for card in subset) == 15:
+                    print(f"{subset_expression} = 15.")
                     points += 2
 
         return points
@@ -35,6 +38,7 @@ class Scoring:
 
         for card1, card2 in itertools.combinations(card_list, 2):
             if card1.value == card2.value:
+                print(f"{card1} and {card2} are pairs.")
                 points += 2
 
         return points
@@ -49,6 +53,7 @@ class Scoring:
             indices = [allowed_values.index(value) for value in values]
 
             if all(indices[i] == indices[i + 1] - 1 for i in range(len(indices) - 1)):
+                print(f"{' '.join(str(card) for card in three_card_combo)} for a three-card run.")
                 points += 3
 
         return points
